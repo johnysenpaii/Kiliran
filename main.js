e777a9c11b7f3e878bdd3ff933e51f00d25f64ff
johnysenpaii <johnjalosjos06@gmail.com>
2022-04-07 18:36:21 +0800

    major changes

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
-var x = document.getElementById("myAudio"); 
 
-function playAudio() { 
-  x.play(); 
-} 
+function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
 
-function pauseAudio() { 
-  x.pause(); 
-} 
+function playPause() {
+    var audio = document.getElementById("audioFile");
+    //var icon = document.getElementById("myIcon");
+    if (audio.paused && document.getElementById("myIcon").className.match(/(?:^|\s)fa-solid fa-play my-float(?!\S)/)){
+        audio.play(); 
+        document.getElementById("myIcon").className = "fa-solid fa-pause my-float";
+    }else{ 
+        audio.pause();
+        document.getElementById("myIcon").className = "fa-solid fa-play my-float";
+    }
+}
